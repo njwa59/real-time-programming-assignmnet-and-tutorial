31256faac3c7f415751d5c19f020d02db7a82dac
alianajwa <njwa2942003@gmail.com>
2025-05-12 15:49:25 +0800

    Add files via upload

---
+++ tutorial/tutorial_02/MainThread.java
@@ -0,0 +1,9 @@
+package Week_02;
+
+public class MainThread {
+    public static void main (String[]args){
+      System.out.println(Thread.currentThread().getName());
+      System.out.println(Thread.currentThread().getId());
+
+    }
+}
